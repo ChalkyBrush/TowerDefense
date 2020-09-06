79f3615ec26ac7d3288bb6dd18a2284e15f2bd41
Ryan Racioppo <ryanracioppo@gmail.com>
2020-09-06 16:06:35 -0400

    initial commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)][SerializeField] float gridSize = 10f;
+
+    TextMesh coordinatesTextMesh;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        coordinatesTextMesh = GetComponentInChildren<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(Mathf.RoundToInt(transform.position.x / gridSize) * gridSize);
+        snapPos.y = 0;
+        snapPos.z = Mathf.RoundToInt(Mathf.RoundToInt(transform.position.z / gridSize) * gridSize);
+
+        coordinatesTextMesh.text = snapPos.x / gridSize + ", " + snapPos.z / gridSize;
+        transform.position = snapPos;
+    }
+}
