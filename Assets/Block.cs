8fcfde492a076882d46d2365eeec8ebf8c37b791
Ryan Racioppo <ryanracioppo@gmail.com>
2020-09-07 16:53:15 -0400

    Update Block.cs

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,15 +8,28 @@ public class Block : MonoBehaviour
     public bool isExplored = false;
     public Block exploredFrom;
 
+    Vector3 towerPlacementOffset = new Vector3(0f, 2.5f, 2f);
     const int gridSize = 6;
     Vector2Int gridPos;
 
+    public bool isPlaceable = true;
+    private bool isMousedOver = true;
+
+
+    private Color mouseOverColor = new Color(0.5f, 0.5f, 0.5f, 1f);
+    private Color baseColor;
+
+    [SerializeField] GameObject towerPrefab;
+    private bool hasTower = false;
+
+    private void Start()
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        baseColor = topMeshRenderer.material.color;
+    }
     private void Update()
     {
-        //if (isExplored)
-        //{
-        //    SetTopColor(Color.black);
-        //}
+        
     }
     public int GetGridSize()
     {
@@ -34,5 +48,38 @@ public class Block : MonoBehaviour
     {
         MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
         topMeshRenderer.material.color = color;
+        baseColor = topMeshRenderer.material.color;
+    }
+
+    public void SetHighlightColor(Color color)
+    {
+        MeshRenderer topMeshRenderer = transform.Find("Top").GetComponent<MeshRenderer>();
+        topMeshRenderer.material.color = color;
+    }
+
+    private void OnMouseOver()
+    {
+        //SetHighlightColor(mouseOverColor);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isPlaceable)
+            {
+                PlaceTower();
+            }
+        }
+    }
+
+    private void PlaceTower()
+    {
+        if (!hasTower)
+        {
+            Instantiate(towerPrefab, transform.position + towerPlacementOffset, Quaternion.identity);
+            hasTower = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        //SetHighlightColor(baseColor);
     }
 }
