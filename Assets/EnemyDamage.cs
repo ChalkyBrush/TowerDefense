dd06f37c04ee8c98dd529df803c50ea12d52559e
Ryan Racioppo <ryanracioppo@gmail.com>
2020-09-07 12:28:50 -0400

    towers shoot

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int hitPoints = 10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("HIT");
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        hitPoints = hitPoints - 1;
+        if (hitPoints <= 0)
+        {
+            DestroyEnemy();
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+}
